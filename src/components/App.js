6a61d53387aa00a99823ea8889e723625d1e97ed
Akshay Chaudhary <124782758+AkshayChaudharyctrl@users.noreply.github.com>
2023-06-20 05:26:44 +0000

    first

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,25 @@
 
-import React from "react";
-import './../styles/App.css';
+import React, { useState, useEffect } from 'react';
 
 const App = () => {
+  const [seconds, setSeconds] = useState(0);
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setSeconds((prevSeconds) => prevSeconds + 1);
+    }, 1000);
+
+    return () => {
+      clearInterval(timer);
+    };
+  }, []);
+
   return (
     <div>
-        {/* Do not remove the main div */}
+      <p>You've been on this Page for {seconds} seconds</p>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
+
